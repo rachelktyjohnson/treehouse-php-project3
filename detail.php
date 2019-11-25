5d15b546e989c86612ed3da45ed541d2898ac561
Rachel <rachelkhoong@gmail.com>
2019-11-25 21:50:14 +1100

    :hammer: Detail page shows required info from DB

--- detail.php
+++ detail.php
@@ -1,36 +1,67 @@
 <?php
+
+require_once('connection.php');
+
+if (isset($_GET['id'])){
+  $id=filter_input(INPUT_GET,'id',FILTER_SANITIZE_NUMBER_INT);
+}
+
+try {
+  $results = $db->prepare('SELECT * FROM entries WHERE id=?');
+  $results->bindParam(1,$id,PDO::PARAM_INT);
+  $results->execute();
+
+  $entry = $results->fetch(PDO::FETCH_ASSOC);
+  if (empty($entry)){
+    header("location:index.php");
+  }
+  //print_r($entry);
+  //die();
+} catch (Exception $e){
+  echo $e->getMessage();
+  die();
+}
 include('./inc/header.php');
+
 ?>
+
 <section>
   <div class="container">
     <div class="entry-list single">
       <article>
-        <h1>The best day I’ve ever had</h1>
-        <time datetime="2016-01-31">January 31, 2016</time>
+        <h1><?= $entry['title']; ?></h1>
+        <?php
+        $vardate = explode('-',$entry['date']);
+        $year = intval($vardate[0]);
+        $month = date('F',mktime(0,0,0,intval($vardate[1])));
+        $date = intval($vardate[2]);
+        ?>
+        <time datetime="<?= $entry['date'] ?>"><?= $month; ?> <?= $date ?>, <?= $year; ?></time>
         <div class="entry">
           <h3>Time Spent: </h3>
-          <p>15 Hours</p>
+          <p><?= $entry['time_spent'] ?></p>
         </div>
         <div class="entry">
           <h3>What I Learned:</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut rhoncus felis, vel tincidunt neque.</p>
-          <p>Cras egestas ac ipsum in posuere. Fusce suscipit, libero id malesuada placerat, orci velit semper metus, quis pulvinar sem nunc vel augue. In ornare tempor metus, sit amet congue justo porta et. Etiam pretium, sapien non fermentum
-            consequat, <a href="">dolor augue</a> gravida lacus, non accumsan. Vestibulum ut metus eleifend, malesuada nisl at, scelerisque sapien.</p>
+          <p><?= $entry['learned'] ?></p>
         </div>
         <div class="entry">
           <h3>Resources to Remember:</h3>
-          <ul>
-            <li><a href="">Lorem ipsum dolor sit amet</a></li>
-            <li><a href="">Cras accumsan cursus ante, non dapibus tempor</a></li>
-            <li>Nunc ut rhoncus felis, vel tincidunt neque</li>
-            <li><a href="">Ipsum dolor sit amet</a></li>
-          </ul>
+          <p>
+          <?php
+          if (!empty($entry['resources'])){
+            echo $entry['resources'];
+          } else {
+            echo "None";
+          }
+          ?>
+          </p>
         </div>
       </article>
     </div>
   </div>
   <div class="edit">
-    <p><a href="edit.html">Edit Entry</a></p>
+    <p><a href="edit.php?id=<?= $id ?>">Edit Entry</a></p>
   </div>
 </section>
 
